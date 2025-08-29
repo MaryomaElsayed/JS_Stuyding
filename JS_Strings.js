25d519d10ce83c7e2d6bf67c3b98eeb43fdac8cd
MaryomaElsayed <maryamelsayd.1@gmail.com>
2025-08-29 21:22:17 +0300

    Strings

---
+++ JS_Strings.js
@@ -0,0 +1,61 @@
+// Strings are for storing text
+// Strings are written with quotes
+
+
+// [1] Single or double quotes can be used:
+let car1 = "BMW";
+let car2 = "Volvo";
+
+// [2] *** Quotes Inside Quotes
+// You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+
+// [3]*** [Best] Template Strings
+// Templates are strings enclosed in backticks (`This is a template string`).
+let text = `He's often called "Johnny"`;
+// Also templates allow multiline strings:
+let text5 =
+`The quick
+brown fox
+jumps over
+the lazy dog`;
+
+
+// [4]*** String Length
+let text2 = "fksnfoaksnpf";
+console.log(text2.length);
+
+// [5]*** Escape Characters
+// The backslash escape character (\) turns special characters into string characters:
+let text3 = "We are the so-called \"Vikings\" from the north.";
+let text4 = "The character \\ is called backslash.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
